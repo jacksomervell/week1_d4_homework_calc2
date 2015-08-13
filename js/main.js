3c5a57bd8ae1ea8d1dd1048362328891688eeb50
Jack Somervell <jacksomervell@gmail.com>
2015-08-13 19:11:16 +0100

    started the trip

--- js/main.js
+++ js/main.js
@@ -37,8 +37,23 @@ var calcButton = document.getElementById("basic-calc");
 
     document.getElementById("basic-answer").className = "show";
     document.getElementById("basic-answer-alert").innerHTML = answer; 
-
-    
-
       
    console.log("clicked!"); })
+
+//trip calculator
+
+var calcButton = document.getElementById("trip-calc");
+  calcButton.addEventListener("click", function(){
+    var dist = document.getElementById("basic-num-1").value
+    var mpg = document.getElementById("basic-operation").value
+    var cost = document.getElementById("basic-num-2").value
+    var speed = document.getElementById("basic-num-2").value
+
+
+
+var time = dist / speed;
+      time = Math.round(time * 100) / 100; // round it to two decimals
+      var actualMPG = (mpg > 60) ? Math.max(0, mpg - (speed - 60) * 2) : mpg;
+
+      var cost = (dist / actualMPG * cost);
+      cost = Math.round(cost * 100) / 100;
