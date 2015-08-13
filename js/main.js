038d8e23ff69d7ddb2db2f6b01b7a06fd56ce3c8
Jack Somervell <jacksomervell@gmail.com>
2015-08-13 18:32:44 +0100

    added in parseFloats

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@ var calcButton = document.getElementById("basic-calc");
 
 
   calcButton.addEventListener("click", function(){
-   var num1 = document.getElementById("col-sm-2").value
+   var num1 = document.getElementById("basic-num-1").value
    var op = document.getElementById("basic-operation").value
-   var num2 = document.getElementById("col-sm-2").value
+   var num2 = document.getElementById("basic-num-2").value
+
       while (isNaN(num1)) {
         num1 = prompt('You must choose an actual number in Number 1!')
       }
       while (isNaN(num2)) {
         num2 = prompt('You must choose an actual number in Number 2!')
       }
+
+      num1 = parseFloat(num1);
+      num2 = parseFloat(num2);
 
       if (op === "+") {
         console.log(num1 + num2);
